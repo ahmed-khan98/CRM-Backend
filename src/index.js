981796db3c5e20687f1e06b380408222e0c3481b
ahmed-khan98 <ahmedkhn015@gmail.com>
2025-12-11 20:51:45 +0500

    'done'

--- src/index.js
+++ src/index.js
@@ -1,46 +1,39 @@
 // require('dotenv').config({path: './env'})
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import {app} from './app.js'
+import { app } from "./app.js";
 
 dotenv.config({
-    path: './.env'
-})
+  path: "./.env",
+});
+
+app.get("/", (req, res) => {
+  console.log("--- RAILWAY HEALTH CHECK HIT SUCCESSFULLY ---");
+  res.send("server running — OK");
+});
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+  });
 
 // UNHANDLED ERRORS KO CATCH KARNE KE LIYE
-process.on('unhandledRejection', (reason, promise) => {
-  console.error('CRITICAL UNHANDLED REJECTION:', reason.message || reason);
+process.on("unhandledRejection", (reason, promise) => {
+  console.error("CRITICAL UNHANDLED REJECTION:", reason.message || reason);
   // process.exit(1); // Isse hata dein taaki server na ruke
 });
 
-process.on('uncaughtException', (err) => {
-  console.error('CRITICAL UNCAUGHT EXCEPTION:', err.message);
+process.on("uncaughtException", (err) => {
+  console.error("CRITICAL UNCAUGHT EXCEPTION:", err.message);
   // process.exit(1);
 });
 
-
-app.get('/',(req,res)=>{
-    res.send('server running — OK')
-})
-
-connectDB()
-.then(() => {
-    app.listen(process.env.PORT , () => {
-        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
-    })
-})
-.catch((err) => {
-    console.log("MONGO db connection failed !!! ", err);
-})
-
-
-
-
-
-
 // "dev": "node -r @babel/register src/index.js",
-
-
 
 /*
 import express from "express"
